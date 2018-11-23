b30bcb85ac76088b67cc292e7a76fe8ce862a5d7
YoniBenZvi <44531536+YoniBenZvi@users.noreply.github.com>
2018-11-23 18:22:33 +0200

    Add files via upload

--- OOP/Solution/CasaDeBurritoImpl.java
+++ OOP/Solution/CasaDeBurritoImpl.java
@@ -2,69 +2,97 @@ package OOP.Solution;
 
 import OOP.Provided.CasaDeBurrito;
 import OOP.Provided.Profesor;
-import java.util.Set;
 
-/*
-    REMEMBER: if you override "equals" you must also override hashcode!!!
- */
-
+import java.util.*;
 
 public class CasaDeBurritoImpl implements OOP.Provided.CasaDeBurrito {
-    public CasaDeBurritoImpl(int id, String name, int dist, Set<String> menu){
+    private int id;
+    private String name;
+    private int dist;
+    private TreeSet<String> menu;
+    private TreeMap<int, int> ratings;
 
+    public CasaDeBurritoImpl(int id_t, String name_t, int dist_t, Set<String> menu_t) {
+        id = id_t;
+        name = name_t;
+        dist = dist_t;
+        menu = new TreeSet<String>();
+        menu.addAll(menu_t);
+        ratings = new TreeMap<int, int>();
     }
 
     /**
      * @return the id of the casa de burrito.
-     * */
-    @Override public int getId(){
-
+     */
+    @Override
+    public int getId() {
+        return this.id;
     }
 
     /**
      * @return the name of the casa de burrito.
-     * */
-    @Override public String getName(){
-
+     */
+    @Override
+    public String getName() {
+        return this.name;
     }
 
     /**
-     * @return the distance from the Technion.*/
-    @Override public int distance(){
-
+     * @return the distance from the Technion.
+     */
+    @Override
+    public int distance() {
+        return this.dist;
     }
 
     /**
-     * @return true iff the profesor rated this CasaDeBurrito
      * @param p - a profesor
-     * */
-    @Override public  boolean isRatedBy(Profesor p){
-
+     * @return true iff the profesor rated this CasaDeBurrito
+     */
+    @Override
+    public boolean isRatedBy(Profesor p) {
+        return this.ratings.containsKey(p.getId());
     }
 
     /**
      * rate the CasaDeBurrito by a profesor
-     * @return the object to allow concatenation of function calls.
+     *
      * @param p - the profesor rating the CasaDeBurrito
      * @param r - the rating
-     * */
-    @Override public OOP.Provided.CasaDeBurrito rate(Profesor p, int r)
-            throws OOP.Provided.CasaDeBurrito.RateRangeException{
-
+     * @return the object to allow concatenation of function calls.
+     */
+    @Override
+    public OOP.Provided.CasaDeBurrito rate(Profesor p, int r)
+            throws OOP.Provided.CasaDeBurrito.RateRangeException {
+        if (r < 0 || r > 5) {
+            throw new RateRangeException();
+        }
+        this.ratings.put(p.getId(), r);
+        return this;
     }
 
     /**
      * @return the number of rating the CasaDeBurrito has received
-     * */
-    @Override public int numberOfRates(){
-
+     */
+    @Override
+    public int numberOfRates() {
+        return this.ratings.size();
     }
 
     /**
      * @return the CasaDeBurrito's average rating
-     * */
-    @Override public double averageRating(){
-
+     */
+    @Override
+    public double averageRating() {
+        if (this.ratings.isEmpty()) {
+            return 0;
+        }
+        List<int> rates = new ArrayList<int>(ratings.values());
+        int sum = 0;
+        for (int i; rates) {
+            sum += i;
+        }
+        return (double) sum / (double) (this.numberOfRates());
     }
 
     /**
@@ -76,21 +104,30 @@ public class CasaDeBurritoImpl implements OOP.Provided.CasaDeBurrito {
      * Menu: <menuItem1, menuItem2, menuItem3...>.
      * </format>
      * Note: Menu items are ordered by lexicographical order, asc.
-     *
+     * <p>
      * Example:
-     *
+     * <p>
      * CasaDeBurrito: BBB.
      * Id: 1.
      * Distance: 5.
      * Menu: Cola, French Fries, Steak.
-     *
-     * */
-    @Override public String toString(){
-
-    }
-
-    @Override public int compareTo(CasaDeBurrito casaDeBurrito){
-
+     */
+    @Override
+    public String toString() {
+        LinkedList<String> menuList = menu.toList();
+        return ("CasaDeBurrito: " + name + ".\nId: " + id + ".\nDistance: " + dist + ".\nMenu: "
+        menuList.stream()
+                .sorted((s1, s2) -> s1.compareTo(s2));
+                .collect(Collectors.toList())
+                .toString()
+                .replace("[", "")
+                .replace("]", "."));
     }
 
 }
+
+    @Override
+    public int compareTo(CasaDeBurrito casaDeBurrito) {
+        return this.getId() - casaDeBurrito.getId();
+    }
+
